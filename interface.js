fd442ed067c0e2fd9995b4cb6e60556ab3e93d5d
Saravanan <hsaravanan29@gmail.com>
2021-07-16 22:29:22 +0530

    Updates

---
+++ interface.js
@@ -0,0 +1,7 @@
+
+function load(){
+    var con = document.querySelectorAll(".editor-container"); 
+    con[0].style.display="block";
+    con[1].style.display = "none";
+    con[2].style.display = "none";
+}
---
+++ precache-manifest.8d5a088bd3343a11c5bd1f257f931fcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0168ca147027d4102d001203f10a8fa7",
+    "url": "/code-editor/index.html"
+  },
+  {
+    "revision": "9e7655981d710f2fc6f2",
+    "url": "/code-editor/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "cc352328604252c139c1",
+    "url": "/code-editor/static/css/main.cc2f4dcb.chunk.css"
+  },
+  {
+    "revision": "9e7655981d710f2fc6f2",
+    "url": "/code-editor/static/js/2.2b60ea5a.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/code-editor/static/js/2.2b60ea5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc352328604252c139c1",
+    "url": "/code-editor/static/js/main.744aff7e.chunk.js"
+  },
+  {
+    "revision": "711f9b6c92f32b083b2d",
+    "url": "/code-editor/static/js/runtime-main.7d8cfe5c.js"
+  }
+]);
